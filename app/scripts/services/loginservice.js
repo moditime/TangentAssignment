48b6339af6e1c25e4b5ddc84accc2ead0c0a0e4b
Moditime <mmoditime@gmail.com>
2017-03-09 13:29:23 +0200

    added login service

---
+++ app/scripts/services/loginservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name tanAppApp.loginService
+ * @description
+ * # loginService
+ * Service in the tanAppApp.
+ */
+angular.module('tanAppApp')
+  .service('loginService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
